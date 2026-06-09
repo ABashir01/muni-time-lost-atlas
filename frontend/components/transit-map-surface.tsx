2212cd000a5637b4ad5c87a29e2c792ccb4bcd9c
ABashir01 <ahadb2020@gmail.com>
2026-06-08 23:10:57 -0700

    Changed to no labels

--- frontend/components/transit-map-surface.tsx
+++ frontend/components/transit-map-surface.tsx
@@ -44,7 +44,7 @@ const mapStyle: StyleSpecification = {
       attribution:
         '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
       tileSize: 256,
-      tiles: ["https://basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png"],
+      tiles: ["https://basemaps.cartocdn.com/light_nolabels/{z}/{x}/{y}.png"],
       type: "raster",
     },
   },
